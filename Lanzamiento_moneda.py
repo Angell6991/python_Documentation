15eb058ee5356d7ad8187b090c17a66b739d0d33
Miguel Angel <miguel9196.guzman@gmail.com>
2023-07-04 21:19:16 -0500

    Lanzamiento moneda

---
+++ Lanzamiento_moneda.py
@@ -0,0 +1,35 @@
+import os
+import math
+import random
+
+os.system('clear')
+
+x,C,S   =   0,0,0
+l       =   float(input("lanzamientos de la moneda\n"))
+
+while ( x < l ):
+    R   =   ((random.randint(0,l))%2)+1
+    #print(R)  # R=1,caras y R=2,sellos #
+
+    if R == 1:
+        C   =   C + R
+    else:
+        S   =   S + ( R / 2 )
+
+    x  =   x + 1
+
+print("Frecuencia de acida")
+print(C,"caras")
+print(S,"sellos")
+print(" ")
+
+Pc  =   ( C / l )
+pcp =   Pc*100
+
+Ps  =   ( S / l )
+psp =   Ps*100
+
+print("Probabilidad y porcentaje")
+print("caras",Pc,"---",pcp,"%")
+print("sellos",Ps,"---",psp,"%")
+
