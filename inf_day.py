21c571ac9ca5c27f1590d0e45473f00a499d0493
Miguel Angel <miguel9196.guzman@gmail.com>
2023-12-11 13:57:53 -0500

    info_day_Exception_Error´s

--- inf_day.py
+++ inf_day.py
@@ -5,31 +5,50 @@ import os
 
 os.system("clear")
 
-#----------------Batery_info------------------#
+###############################################
+###-------------Batery_info-----------------###
+###############################################
 inf_batery  =   ps.sensors_battery()
 porcent     =   inf_batery.percent
 batery      =   int(porcent)
 
-#-----------------Time_info-------------------#
+###############################################
+###---------------Time_info-----------------###
+###############################################
 inf_time    =   tm.datetime.now()
 fecha       =   inf_time.strftime("%d/%m/%Y")
 hora        =   inf_time.strftime("%H:%M")
 year        =   inf_time.strftime("%Y")
 month       =   inf_time.strftime("%m")
 
-#------------------Day_info-------------------#
+###############################################
+###----------------Day_info-----------------###
+###############################################
 inf_cal = cl.TextCalendar(cl.SUNDAY)
 
-mes_0 = inf_cal.formatmonth(int(year), int(month)-1)
+#--------------Previous_Month-----------------#
+try:
+    mes_0 = inf_cal.formatmonth(int(year), int(month) -1)
+except IndexError:
+    mes_0 = inf_cal.formatmonth(int(year)-1, 12)
+
+#---------------Current_Month-----------------#
 mes_1 = inf_cal.formatmonth(int(year), int(month))
-mes_2 = inf_cal.formatmonth(int(year), int(month)+1)
 
-#------------------Viw_info-------------------#
-# print(f"{mes_0} \n {mes_1} \n {mes_2}")
-# print(f"    {batery}%     {hora}      {fecha}  ")
+#----------------Next_Month-------------------#
+try:
+    mes_2 = inf_cal.formatmonth(int(year), int(month) +1)
+except IndexError:
+    mes_2 = inf_cal.formatmonth(int(year)+1, 1)
 
-text_1  = "\033[37m" + f"{mes_0} \n" + "\033[30m" +  f"{mes_1} \n" + "\033[37m" +  f"{mes_2}"
-text_2  = "\033[37m" + "  " + "\033[32m"  + "  " + "\033[30m" +  f"{batery}" + "%" + "\033[37m" + "    " + "\033[32m" + "   " + "\033[30m" + f"{hora}" + "\033[37m" + "    " + "\033[32m" + "   " + "\033[30m" + f"{fecha}" + "\033[37m" + "  "
+###############################################
+###---------------Viw_info------------------###
+###############################################
+
+text_1  = "\033[36m" + f"{mes_0} \n" + "\033[35m" +  f"{mes_1} \n" + "\033[36m" +  f"{mes_2}"
+text_2  = "\033[37m" + " " + "\033[32m"  + "  " + "\033[39m" +  f"{batery}" + "%" + "\033[37m" + "  " + "\033[32m" + "  " + "\033[39m" + f"{hora}" + "\033[37m" + " " + "\033[32m" + "   " + "\033[39m" + f"{fecha}" + "\033[37m" + " "
 
 print(text_1)
 print(text_2)
+
+
