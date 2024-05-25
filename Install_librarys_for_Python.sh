3ecbd958a04f4546167a9eb4363a23d26e2a023a
Miguel Angel <miguel9196.guzman@gmail.com>
2024-05-25 16:29:29 -0500

    Scrit_librarys_python

---
+++ Install_librarys_for_Python.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#---instalando_librerias---#
+
+echo "Instalando librerias Python:"
+
+pip install numpy sympy pandas pylatex pyinstaller einsteinpy matplotlib scikit-learn scipy
+
+clear
+
+pip list
+
+echo "Instalación completada"
